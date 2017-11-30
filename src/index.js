a1acb6136f594e721f70f1e07487dc4f2e78ca90
DudkoSerhei <serhei.dudko@gmail.com>
2017-11-30 03:33:54 +0300

    Add provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { todos } from './reducers/todos';
 import { visibilityFilter } from './reducers/visibilityFilter';
+import PropTypes from 'prop-types';
 import App from './App.jsx';
 
 import './index.css';
@@ -11,12 +12,26 @@ const todoApp = combineReducers({
   todos,
   visibilityFilter
 });
-const store = createStore(todoApp);
 
-const render = () => {
-  ReactDOM.render(
-    <App todos={store.getState().todos} />,
-    document.getElementById('root'));
+class Provider extends Component {
+  getChildContext() {
+    return {
+      store: this.props.store
+    };
+  }
+
+  render() {
+    return this.props.children;
+  }
 }
-store.subscribe(render);
-render();
+
+ReactDOM.render(
+    <Provider store={createStore(todoApp)}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
+Provider.childContexTypes = {
+    store: PropTypes.object
+}
